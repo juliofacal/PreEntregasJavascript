8e879d8346e8718bbca0cb812b7b7b27b9c9eb53
Julio Facal <facaljuliojuanemilio@gmail.com>
2024-12-03 20:26:20 -0300

    borrado de producto implementado

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,21 @@
+/*
+Los productos del catálogo son cargados desde el JSON, si es que el item 
+"catalogo" en el local storage está vacío. Toda actualización hecha desde el ABM
+se hace sobre los datos en el local storage. En esta implementación, existe la
+consecuencia que si se elimina todo el catálogo, se populará nuevamente desde el
+JSON. No vi utilidad en poner validaciones extras para evitar esto, si es que la
+verdadera solución sería conectar a una BBDD verdadera. Esto siendo un concepto
+de prueba, es suficiente a mi parecer.
+*/
+
+const catalogo = JSON.parse(localStorage.getItem("catalogo")) || [];
+const contenedorABMCatalogo = document.getElementById("contenedorABMCatalogo") || [];
+const botonOfertas = document.getElementById("ofertas");
+const botonHabilitados = document.getElementById("habilitados");
+const botonSuspendidos = document.getElementById("suspendidos");
+const botonTodos = document.getElementById("todos");
+const barraBusqueda = document.getElementById("busqueda");
+
 class Producto {
     constructor(id, nombre, precio, descripcion, img) {
         this.id = id;
@@ -8,14 +26,6 @@ class Producto {
         this.oferta = false;
     }
 }
-
-const catalogo = JSON.parse(localStorage.getItem("catalogo")) || [];
-const contenedorABMCatalogo = document.getElementById("contenedorABMCatalogo") || [];
-const botonOfertas = document.getElementById("ofertas");
-const botonHabilitados = document.getElementById("habilitados");
-const botonSuspendidos = document.getElementById("suspendidos");
-const botonTodos = document.getElementById("todos");
-const barraBusqueda = document.getElementById("busqueda");
 
 const renderizarABMProductos = (array) => {
     contenedorABMCatalogo.innerHTML = "";
@@ -29,62 +39,66 @@ const renderizarABMProductos = (array) => {
         <h3>${producto.nombre}</h3>
         <p>$${producto.precio}</p>
         <img src="${producto.img}" alt="${producto.descripcion}">
-        <button type=button id="suspenderProducto(${producto.id})" ${(producto.habilitado)? "" : "disabled"}>Suspender</button>
-        <button type=button id="reanudarProducto(${producto.id})" ${(!producto.habilitado)? "" : "disabled"}>Reanudar</button>
-        <button type=button id="eliminarProducto(${producto.id})">Eliminar</button>
+        <button type=button onclick="suspenderProducto(${producto.id});" ${(producto.habilitado)? "" : "disabled"}>Suspender</button>
+        <button type=button onclick="reanudarProducto(${producto.id});" ${(!producto.habilitado)? "" : "disabled"}>Reanudar</button>
+        <button type=button onclick="borrarProducto(${producto.id});">Eliminar</button>
         `;
 
         contenedorABMCatalogo.appendChild(div);
     });
 }
 
-const agregarProductos = () => {
-    let mensaje = "No se pudo agregar el producto.";
-    let nombre = "";
-    let precio = "";
-    let descripcion = "";
-
-    do {
-        nombre = prompt("Ingrese nombre del producto.");
-        if (nombre === null)
-            return mensaje;
-    } while (nombre === "");
-    do {
-        precio = prompt("Ingrese precio del producto.");
-        if (precio === null)
-            return mensaje;
-    } while (precio === "");
-    do {
-        descripcion = prompt("Ingrese descripción del producto.");
-        if (descripcion === null)
-            return mensaje;
-    } while (descripcion === "");
-
-    let producto = new Producto(nombre, precio, descripcion);
-
-    productos.push(producto);
-    mensaje = "Producto agregado correctamente.";
-
-    return mensaje;
+const agregarProductos = (id, nombre, precio, descripcion, img) => {
+    let mensaje = "Confirme la creación y agregado del producto al catálogo.";
+    if (confirm(mensaje)) {
+        const producto = new Producto(id, nombre, precio, descripcion, img);
+        catalogo.push(producto);
+        Toastify({
+            text: "Producto agregado.",
+            duration: 5000,
+            close: true,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function(){} // Callback after click
+        }).showToast();
+    }
 }
 
-const borrarProducto = (productoABorrar) => {
-    let mensaje = "No se encontró el producto.";
-    for (let i = 0; i < productos.length; i++) {
-        if (productos[i].nombre === productoABorrar) {
-            productos.splice(i);
-            mensaje = `Producto "${productoABorrar}" fue eliminado.`
+const borrarProducto = (id) => {
+    let mensaje = "Confirme el borrado del producto del catálogo."
+
+    if (confirm(mensaje)) {
+        let index = catalogo.map(element => element.id).indexOf(id);
+        if (index !== -1) {
+            catalogo.splice(index, 1);
+            guardarCatalogo();
+            renderizarABMProductos(catalogo);
+            Toastify({
+                text: "Producto borrado.",
+                duration: 5000,
+                close: true,
+                gravity: "bottom", // `top` or `bottom`
+                position: "center", // `left`, `center` or `right`
+                stopOnFocus: true, // Prevents dismissing of toast on hover
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                },
+                onClick: function(){} // Callback after click
+            }).showToast();
         }
-    }
-    return mensaje;
+    } 
 }
 
 const modificarProducto = (productoAModificar) => {
     let mensaje = "No se encontró el producto.";
-    for (let i = 0; i < productos.length; i++) {
-        if (productos[i].nombre === productoAModificar) {
-            productos[i].precio = prompt("Ingrese el nuevo precio del producto.");
-            productos[i].descripcion = prompt("Ingrese la nueva descripción del producto.");
+    for (let i = 0; i < catalogo.length; i++) {
+        if (catalogo[i].nombre === productoAModificar) {
+            catalogo[i].precio = prompt("Ingrese el nuevo precio del producto.");
+            catalogo[i].descripcion = prompt("Ingrese la nueva descripción del producto.");
             mensaje = `Producto "${productoAModificar}" fue actualizado.`
         }
     }
@@ -119,6 +133,10 @@ barraBusqueda.addEventListener("input", () => {
 
     renderizarABMProductos(productosFiltrados)
 });
+
+const guardarCatalogo = () => {
+    localStorage.setItem("catalogo", JSON.stringify(catalogo));
+};
 
 window.addEventListener("DOMContentLoaded", (e) => {
     if (catalogo === undefined || catalogo.length == 0) {
